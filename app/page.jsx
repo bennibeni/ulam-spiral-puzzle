211f26b945174b5c2cdf58ae05520dc88ce7738a
bennibeni <113261740+bennibeni@users.noreply.github.com>
2026-09-15 06:17:37 +0200

    Updated footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
 import UlamSpiralPuzzle from "./UlamSpiralPuzzle";
 
 export default function Page() {
-  return <UlamSpiralPuzzle />;
+  return (
+    <main >
+      <UlamSpiralPuzzle />
+      <footer className="projects-footer">
+        <a href="https://links-page-bennibeni.vercel.app/">&larr; All projects</a>
+      </footer>
+    </main >
+  );
 }
